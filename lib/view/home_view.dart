2b85b4be8018cf56bc70da42e9e2f912d97566f7
metehanpinarli <meteerkan1919@gmail.com>
2021-10-09 04:52:20 +0300

    post score

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class _HomeViewState extends State<HomeView> {
   var typedCharLength = 0;
   var lastTypeAt;
   var userName;
+  var url = Uri.parse('https://klavyeyarisiapi.herokuapp.com/users/score');
   final userNameController = TextEditingController();
   var lorem =
       "                                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor nec arcu et interdum. Donec sit amet nibh neque. Aenean lacinia nulla a ornare dictum. Nunc euismod dignissim lacus vitae efficitur. Morbi in leo est. Proin vehicula ante eget tortor imperdiet ultricies. Sed non sem finibus, facilisis diam vitae, tincidunt est. Etiam blandit sed justo eu pretium. Nam mollis lorem tortor."
@@ -21,7 +24,7 @@ class _HomeViewState extends State<HomeView> {
           .replaceAll(",", '')
           .replaceAll('.', '');
 
-  void onStartClick() {
+  void onStartClick() async {
     userName = userNameController.text;
     print(userName);
     setState(() {
@@ -31,7 +34,10 @@ class _HomeViewState extends State<HomeView> {
     Timer.periodic(Duration(seconds: 1), (timer) {
       int now = DateTime.now().millisecondsSinceEpoch;
       setState(() {
-        if (step == 1 && now - lastTypeAt > 3000) step++;
+        if (step == 1 && now - lastTypeAt > 3000) {
+          step++;
+          http.post(url, body: {'userName': userName.toString(), 'score': typedCharLength.toString()});
+        }
       });
     });
   }
@@ -39,10 +45,9 @@ class _HomeViewState extends State<HomeView> {
   updateLastTypedAt() {
     this.lastTypeAt = DateTime.now().millisecondsSinceEpoch;
   }
-  aa(String a){
-    setState(() {
 
-    });
+  aa(String a) {
+    setState(() {});
   }
 
   onType(String value) {
@@ -51,6 +56,7 @@ class _HomeViewState extends State<HomeView> {
     setState(() {
       if (trimmedValue.indexOf(value) != 0) {
         step = 2;
+        http.post(url, body: {'userName': userName.toString(), 'score': typedCharLength.toString()});
       } else {
         typedCharLength = value.length;
       }
@@ -76,16 +82,14 @@ class _HomeViewState extends State<HomeView> {
       return [
         Text("Hoşgeldin Adın Nedir?"),
         TextField(
-          onChanged:aa,
+          onChanged: aa,
           controller: userNameController,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: "Adın",
           ),
         ),
-        ElevatedButton(
-            onPressed: userNameController.text.length < 2 ? null : onStartClick,
-            child: Text("başla"))
+        ElevatedButton(onPressed: userNameController.text.length < 2 ? null : onStartClick, child: Text("başla"))
       ];
     else if (step == 1) {
       return [
